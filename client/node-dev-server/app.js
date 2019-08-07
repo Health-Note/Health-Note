af558ee6e3487b842eccf6045c516b301c964943
Jongyeol <pjr159@gmail.com>
2019-08-07 17:52:32 +0900

    드래그앤드롭 CRUD 기능 완성. input outpu JSON형식 모두 기획된 형식에 맞게 완성

--- client/node-dev-server/app.js
+++ client/node-dev-server/app.js
@@ -1,22 +1,37 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 app.get("/getData", (req, res) => {
     res.json({
-        members: [{
+         members: [{
+            name: "park",
             phoneNum: "01020777538",
-            startTime: "20190806",
-            endTime: "20190806"
+            startDay: "20190807",
+            startTime: "1600"
         },{
+            name: "jong",
             phoneNum: "01094325615",
-            startTime: "20190806",
-            endTime: "20190806"
+            startDay: "20190806",
+            startTime: "1700"
         },{
+            name: "yeol",
             phoneNum: "01097045552",
-            startTime: "20190806",
-            endTime: "20190806"
+            startDay: "20190808",
+            startTime: "1800"
         }]
     })
+})
+
+// 0성공 1실패
+app.post("/changeMemberSchedule", (req, res) => {
+    console.log(req.body)
+    res.json({result: 0})
+})
+
+app.post("/deletePT", (req, res) => {
+    console.log(req.body)
+    res.json({result: 0})
 })
 
 app.listen(8080, () => {
--- client/src/component/Calendar.js
+++ client/src/component/Calendar.js
@@ -11,20 +11,21 @@ import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Calendar.css";
-import moment from 'moment'
+import moment from 'moment';
 
 class Calendar extends Component {
 
+  // state에서 날짜형식은 ISO // 2017-03-16T17:40:00+09:0
   state = {
     calendarEvents: [
       {
-        title: "Atlanta Monster",
+        title: "kim",
         start: new Date("2019-08-04 16:00"),
         id: "01011112222"
       },
       {
         title: "My Favorite Murder",
-        start: new Date("2019-08-05 00:00"),
+        start: moment("20190804 1630").format(),
         id: "01045678945"
       }
     ],
@@ -38,20 +39,31 @@ class Calendar extends Component {
   };
 
   /**
+   * 작성자: 박종열
+   * 기능 : 서버에서 사용자 이름, 폰넘버, 시작날짜, 시작시간을 받아옴
    * adding dragable properties to external events through javascript
+   * 날짜 : 2019-08-07
    */
-  componentDidMount() {
 
+  componentDidMount() {
     fetch("/getData", {
-      method: "POST",
+      method: "get",
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       }
     }).then((res) => {
-      res.json()
+      return res.json()
     }).then((data) => {
-      console.log(data)
+      let meberList = data.members.map(member => {
+        const startReady = member.startDay + " " + member.startTime;
+        const start = moment(startReady).format();
+        return { title: member.name, id: member.phoneNum, start }
+      })
+      console.log(meberList)
+      this.setState({
+        calendarEvents: this.state.calendarEvents.concat(meberList)
+      })
     })
 
     let draggableEl = document.getElementById("external-events");
@@ -68,33 +80,42 @@ class Calendar extends Component {
     });
   }
 
+  /**
+   * 작성자: 박종열
+   * 기능 : 드래그 드롭시마다 사용자 이름, 폰넘버, 예전시간, 바뀐시간을 받아옴, 
+   *        날짜변환: 20190804 + 1630 => 2019-08-04 16:30
+   * 날짜 : 2019-08-07
+   */
+
   drop = (info) => {
-    console.log(info.event.title)
-    console.log(info.event.id)
-    console.log(moment(info.event.start).format('YYYYMMDD')) // 한글시간으로 변화 필요
-    console.log(info.oldEvent.start.format('YYYYMMDD'))
-    
+    const name = info.event.title;
+    const id = info.event.id;
+    const oldDate = moment(info.oldEvent.start).format('YYYYMMDD');
+    const changedDate = moment(info.event.start).format('YYYYMMDD'); // 한글시간으로 변화 필요
+    const oldTime = moment(info.oldEvent.start).format('HHmm');
+    const changedTime = moment(info.event.start).format('HHmm');
+   
     fetch("/changeMemberSchedule", {
       method: "POST",
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body:JSON.stringify({"name": "pakr"})
+      body: JSON.stringify({phoneNum: id, oldDate, changedDate, oldTime, changedTime})
     }).then((res) => {
-      res.json()
-    }).then((data) => {
-      console.log(data)
+      return res.json()
+    }).then((result) => { //성공시 응답 0, 실패시 1
+      console.log(result)
     })
-
   }
 
-  // eventDrop = (info) => {
-  //   console.log(info)
-  // }
 
-  /**
-   * 얼러트 창
+
+ /**
+   * 작성자: 박종열
+   * 기능 : 캘린더에서 스케줄 클릭시 얼러트창 생성
+   *        삭제클릭시 phoneNum, date, startTime 서버로 보냄
+   * 날짜 : 2019-08-07
    */
   eventClick = eventClick => {
     Alert.fire({
@@ -104,13 +125,13 @@ class Calendar extends Component {
       <table class="table">
       <tbody>
       <tr >
-      <td>title</td>
+      <td>이름</td>
       <td><strong>` +
         eventClick.event.title +
         `</strong></td>
       </tr>
       <tr >
-      <td>Start Time</td>
+      <td>시작</td>
       <td><strong>
       ` +
         eventClick.event.start +
@@ -124,13 +145,31 @@ class Calendar extends Component {
       showCancelButton: true,
       confirmButtonColor: "#d33",
       cancelButtonColor: "#3085d6",
-      confirmButtonText: "Remove Event",
-      cancelButtonText: "Close"
+      confirmButtonText: "삭제",
+      cancelButtonText: "닫기"
     }).then(result => {
       if (result.value) {
         eventClick.event.remove(); // It will remove event from the calendar
+        Alert.fire("삭제성공!", "스케줄에서 삭제되었습니다", "success");
 
-        Alert.fire("Deleted!", "Your Event has been deleted.", "success");
+        const name = eventClick.event.title;
+        const id = eventClick.event.id;
+        const date = moment(eventClick.event.start).format("YYYYMMDD"); // 20190807
+        const startTime = moment(eventClick.event.start).format("HHmm"); // 1830
+    
+        fetch("/deletePT", {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({phoneNum: id, date, startTime})
+        }).then((res) => {
+          return res.json()
+        }).then((result) => { //성공시 응답 0, 실패시 1
+          console.log("삭제결과:", result)
+        })
+
       }
     });
   };
@@ -169,7 +208,6 @@ class Calendar extends Component {
           <Col lg={9} sm={9} md={9}>
             <div className="demo-app-calendar" id="mycalendartest">
               <FullCalendar
-                titleFormat='yyyymmdd'
                 minTime={"06:00:00"}
                 defaultView="dayGridMonth"
                 header={{
