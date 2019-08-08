b50dd7163f02c36f71dd8b53d0195f2d1bff4b7c
Jongyeol <pjr159@gmail.com>
2019-08-08 11:09:57 +0900

    no message

--- client/src/components/Routine/Form.jsx
+++ client/src/components/Routine/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Grid from '@material-ui/core/Grid'
+
 
 const Form = ({ onSubmit }) => {
     const [name, setText] = useState("");
@@ -17,14 +17,14 @@ const Form = ({ onSubmit }) => {
 
     return (
         <div>
-            <Grid item xs={5}>
+        
             <form onSubmit={handleSubmit}>
                 <input type="text" name="name" value={ name } onChange={ handleChange }></input>
                 <input type="text" name="sets" value={ sets } onChange={ handleChange }></input>
                 <input type="text" name="reps" value={ reps } onChange={ handleChange }></input>
                 <button>제출</button>
             </form>
-            </Grid>
+            
         </div>
     )
 }
