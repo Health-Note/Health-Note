6215dcddb0aaa20b92cfd708d6e6c3e7548b95d3
Jongyeol <pjr159@gmail.com>
2019-08-09 16:55:39 +0900

    캘린더에서 회원 클릭시 "날짜", "폰넘버"를 Routine컴포넌트 내에서 공유할 수 있도록 완료.

--- client/src/contexts/ExerciseContext.js
+++ client/src/contexts/ExerciseContext.js
@@ -4,12 +4,16 @@ import Routine from '../components/routine/Routine'
 const ExerciseContext = createContext();
 
 const ExerciseProvider = ( props ) => {
-    const [selectedExer, setSelectedExer] = useState(""); //() => {}
 
+    const [selectedExer, setSelectedExer] = useState(""); //() => {}
+    const [id, setId] = useState("");
+    const [date, setDate] = useState("")
+    const [startTime, setStartTime] = useState("") 
+    
    
-        return(
-            <>
-            <ExerciseContext.Provider value={[selectedExer, setSelectedExer]}>
+    return (
+        <>
+            <ExerciseContext.Provider value={{selectedExer, setSelectedExer, id, setId, date, setDate, startTime, setStartTime}}>
                 {props.children}
             </ExerciseContext.Provider>
         </>
