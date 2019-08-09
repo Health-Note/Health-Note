cfa40f100ca90ba13a2f06a8c09e602ec0824ef5
J K <surhommekim@gmail.com>
2019-08-09 19:41:20 +0900

    정적 스케줄에 저장시 동적 테이블에 같이 싱크를 맞춰주면서 insert하는 로직 개발 끝 동시에 해당 시간에 동적 스케줄 테이블에 누군가 일정이 기록되어 있으면 이를 가져와서 클라이언트에 던져주고 이 구간만 피해서 insert하도록 조치

---
+++ src/main/java/com/healthnote/vo/CheckDayOfScheduleDTO.java
@@ -0,0 +1,24 @@
+package com.healthnote.vo;
+
+public class CheckDayOfScheduleDTO {
+
+	private int day;
+	private String date;
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+}
