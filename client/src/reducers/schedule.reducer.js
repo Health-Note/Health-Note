821c27bc6ea9a6cca90fed32cbfbe407aaea87a6
박종열/강남커넥츠팀 <jongyeol@stunitas.com>
2020-01-03 14:26:16 +0900

    스케줄 리듀서, 멤버리듀서  immer 적용

--- client/src/reducers/schedule.reducer.js
+++ client/src/reducers/schedule.reducer.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {
   SET_SCHEDULE,
   GET_SCHEDULES,
@@ -7,76 +8,55 @@ import {
   CREATE_ONE_SCHEDULE,
 } from './types';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case SET_SCHEDULE_TARGET:
-      return {
-        selectedSchedule: {
-          scheduleId: action.payload.scheduleId,
-          memberId: action.payload.memberId,
-        },
-        schedules: state.schedules.map(schedule => {
-          if (schedule.id === action.payload.scheduleId) {
-            return {
-              ...schedule,
-              target: true,
-              borderColor: 'blue',
-            };
-          } else {
-            return {
-              ...schedule,
-              target: false,
-              borderColor: 'white',
-            };
-          }
-        }),
-      };
-
-    case GET_SCHEDULES:
-      const allSchedules = action.payload;
-      const schedules = allSchedules.reduce((acc, cv) => acc.concat(cv), []); // 이중배열 => 일차원배열
-      return {
-        ...state,
-        schedules,
-      };
-    case SET_SCHEDULE:
-    case CREATE_ONE_SCHEDULE:
-      return {
-        ...state,
-        schedules: [...state.schedules, action.payload],
-      };
-    case UPDATE_SCHEDULE:
-      return {
-        ...state,
-        schedules: state.schedules.map(schedule => {
-          if (schedule.id === action.payload.ScheduleId) {
-            return {
-              ...schedule,
-              start: action.payload.StartTime,
-            };
-          } else {
-            return schedule;
-          }
-        }),
-      };
-
-    case 'TOGGLE':
-      console.log(action.id);
-      return state.map(schedule =>
-        schedule.phonenum + schedule.date === action.id
-          ? { ...schedule, finish_dncd: !schedule.finish_dncd }
-          : schedule
-      );
-    case REMOVE_SHCEDULE:
-      return {
-        ...state,
-        schedules: state.schedules.filter(
-          schedule => parseInt(action.payload) !== schedule.id
-        ),
-      };
-    default:
-      return state;
-  }
-};
+const reducer = (state, action) =>  {
+      switch (action.type) {
+        case SET_SCHEDULE_TARGET:
+          return produce(state, draft => {
+            draft.selectedSchedule.scheduleId = action.payload.scheduleId;
+            draft.selectedSchedule.memberId = action.payload.memberId;
+            draft.schedules.forEach(schedule => {
+              if (schedule.id === action.payload.scheduleId) {
+                schedule.target = true;
+                schedule.borderColor = 'blue';
+              } else {
+                schedule.target = false;
+                schedule.borderColor = 'white';
+              }
+            })
+          });
+        case GET_SCHEDULES:
+          return produce(state, draft => {
+            const allSchedules = action.payload;
+            draft.schedules = allSchedules.reduce((acc, cv) => acc.concat(cv), []); // 이중배열 => 일차원배열
+          });
+        case SET_SCHEDULE:
+        case CREATE_ONE_SCHEDULE:
+          return produce(state, draft => {
+            draft.schedules.push(action.payload);
+          });
+        case UPDATE_SCHEDULE:
+          return produce(state, draft => {
+            draft.schedules.forEach(schedule => {
+              if (schedule.id === action.payload.ScheduleId) {
+                  schedule.start = action.payload.StartTime
+              }
+            })
+          });
+        case 'TOGGLE':
+          return state.map(schedule =>
+              schedule.phonenum + schedule.date === action.id
+                  ? {...schedule, finish_dncd: !schedule.finish_dncd}
+                  : schedule
+          );
+        case REMOVE_SHCEDULE:
+          return produce(state, draft => {
+            draft.schedules = state.schedules.filter(
+                schedule => parseInt(action.payload) !== schedule.id
+            )
+          });
+        default:
+          return state;
+      }
+    };
 
 export default reducer;
