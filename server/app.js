9189498356000917643da9a80febc577b0f325b5
jongyeol <pjr159@gmail.com>
2020-01-03 18:26:04 +0900

    statistic routes 의논

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const authRoutes = require('./routes/auth');
 const scheduleRoutes = require('./routes/schedules');
 const routineRoutes = require('./routes/routine');
 const exerciseRoutes = require('./routes/exercises');
+const statisticsRoutes = require('./routes/statistics');
 
 // 미들웨어
 app.use(express.json());
@@ -18,6 +19,7 @@ app.use('/api/members', memberRoutes);
 app.use('/api/schedules', scheduleRoutes);
 app.use('/api/routine', routineRoutes);
 app.use('/api/exercises', exerciseRoutes);
+app.use('/api/statistics', statisticsRoutes);
 
 app.listen(8080, () => {
   console.log('client-dev-server (express) started');
---
+++ server/routes/statistics.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const db = require('../models_aws/index');
+
+
+router.get('/', async () => {
+  console.log("옴");
+  // const { memberId } = req.body;
+  const result = await db.Routine.findAndCountAll({
+    where: {
+      MemberId: 21,
+      ExerciseCode: {
+        [Op.between]: [100, 199]
+      },
+    },
+  });
+  console.dir(result);
+});
+
+module.exports = router;
+
+// 가슴
+// 등
