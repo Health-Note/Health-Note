9b9fca39127a4974de2c9c6eee2a343a9ddd8b08
jongyeol <pjr159@gmail.com>
2020-06-05 18:23:19 +0900

    [루틴] api 형식의 state 완성, 삭제번호 중복입력 미완성

--- client/src/components/pages/schedule/routine/RoutineList.jsx
+++ client/src/components/pages/schedule/routine/RoutineList.jsx
@@ -1,60 +1,39 @@
-import React from 'react';
-import { Card, Avatar, InputNumber , Row, Tag, Table, Space } from 'antd';
+import React, { useState } from 'react';
+import { Card, Col, Button, InputNumber, Row, Tag, Table, Space } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { v4 as uuid } from 'uuid';
 
 const { Meta } = Card;
-const RoutineList = ({ routines }) => {
+const RoutineList = ({ routine, insertCounts, getDelExerCode }) => {
 
-  const columns = [
-    {
-      title: '부위',
-      dataIndex: 'targetName',
-      key: 'targetName',
-      render: text => <a>{text}</a>,
-    },
-    {
-      title: '운동이름',
-      dataIndex: 'exerciseName',
-      key: 'exerciseName',
-    },
-    {
-      title: '세트수',
-      render: () => (
-        <>
-        <InputNumber  style={{width: '60px'}}/>
-        </>
-      )
-    },
-    {
-      title: '반복수',
-      render: () => (
-        <>
-          <InputNumber  type={'number'} style={{width: '60px'}}/>
-        </>
-      )
-    },
-    {
-      title: '부위',
-      key: uuid(),
-      dataIndex: 'targetName',
-      render: (text) => (
-        <>
-          <Tag color={'red'} key={uuid()}>
-            {text}
-          </Tag>
-        </>
-      ),
-    },
-  ];
+  const [count, setCount] = useState(0);
+  const [repetition, setRepetition] = useState(0);
+
+  const onChangeCount = value => {
+    setCount(value);
+    insertCounts(routine.uuid, count, repetition);
+  };
+
+  const onChangeRepetition = value => {
+    setRepetition(value);
+    insertCounts(routine.uuid, count, repetition);
+  };
+
+  const onClickDeleteButton = () => {
+    getDelExerCode(routine.exerciseCode);
+  };
 
   return (
-    <>
-      <Table columns={columns} dataSource={routines}/>
-    </>
+    <Row justify="start" align="middle"
+         style={{ width: 500, height: 55, margin: 13, padding: 0, border: '1px solid lightgrey' }}>
+      <Col md={3}><Tag>{routine.targetName}</Tag></Col>
+      <Col md={8}>{routine.exerciseName}</Col>
+      <Col md={3}><InputNumber onChange={onChangeRepetition} style={{ width: 55 }}/></Col>
+      <Col md={3}><InputNumber onChange={onChangeCount} style={{ width: 55 }}/></Col>
+      <Col md={3}><Button onClick={onClickDeleteButton} style={{ width: 55 }}>삭제</Button></Col>
+    </Row>
+
   );
-
-
 };
 
 export default RoutineList;
--- client/src/components/pages/schedule/routine2/Routine.jsx
+++ client/src/components/pages/schedule/routine2/Routine.jsx
@@ -1,26 +1,68 @@
 import React, { useState } from 'react';
 import ExerciseSelect from '../routine/ExerciseSelect';
 import RoutineList from '../routine/RoutineList';
+import { v4 as uuid } from 'uuid';
+import { Button } from 'antd';
 
-const Routine = () => {
-  const [routines, setRoutines] = useState([]);
+const Routine = ({ saveRoutines }) => {
+  const [updateRoutines, setUpdateRoutines] = useState([]);
+  const [delExerCode, setDelExerCode] = useState([]);
 
   const getExerIdAndName = (exerciseCode, exerciseName, targetCode, targetName) => {
-    setRoutines(prevState => ([
+    setUpdateRoutines(prevState => ([
       ...prevState,
       {
-        exerciseCode,
-        exerciseName,
-        targetCode,
-        targetName,
+        uuid: uuid(),
+        exerciseCode: exerciseCode,
+        exerciseName: exerciseName,
+        targetCode: targetCode,
+        targetName: targetName,
+        isCardio: 0,
+        setCount: 0,
+        repetitions: 0,
+        maxWeight: 0,
       },
     ]));
+  };
+
+  const insertCounts = (uuid, setCount, repetitions) => {
+    const result = updateRoutines.map(routine => {
+      if (routine.uuid === uuid) {
+        return {
+          ...routine,
+          setCount: setCount,
+          repetitions: repetitions,
+        };
+      } else {
+        return {
+          ...routine,
+        };
+      }
+    });
+    setUpdateRoutines(result);
+  };
+
+  const getDelExerCode = (exerciseCode) => {
+    setDelExerCode([...delExerCode, exerciseCode]);
+  };
+
+  const onClickSave = () => {
+    saveRoutines(delExerCode, updateRoutines);
   };
 
   return (
     <>
       <ExerciseSelect getExerIdAndName={getExerIdAndName}/>
-      <RoutineList routines={routines}/>
+      {updateRoutines.map(routine => {
+        return (
+          <>
+            <RoutineList key={routine.uuid} routine={routine} insertCounts={insertCounts}
+                         getDelExerCode={getDelExerCode}/>
+          </>
+        );
+      })
+      }
+      <Button onClick={onClickSave}>저장</Button>
     </>
   );
 };
