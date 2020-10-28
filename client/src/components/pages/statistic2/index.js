89722796969a696665babc37ef824c578eafe6d6
jongyeol <pjr159@gmail.com>
2020-10-28 19:07:29 +0900

    [통계] 멤버 정보 받아오기 수정중

--- client/src/components/pages/statistic2/index.js
+++ client/src/components/pages/statistic2/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
+import { Select } from 'antd';
+import CustomRadarChart from '../../context/molecules/CustomRadarChart';
 
-const Statistics = ({member: curMember, history}) => {
+const { Option } = Select;
+
+const Statistics = ({ member: curMember, history }) => {
     const { members } = useSelector(state => state.member);
     console.log('curMember', curMember);
     if (typeof curMember === 'undefined') {
         curMember = members[0];
     }
 
+    function handleChange(value) {
+        console.log(`selected ${value}`);
+    }
+
     return (
-        <>
-        <h1>{curMember.memberName}</h1>
-        </>
+      <>
+          <h1>{curMember.memberName}</h1>
+          <Select defaultValue={curMember.memberName} style={{ width: 120 }} onChange={handleChange}>
+              {members.map(cv => (
+                <Option value={cv.memberId}>{cv.memberName}</Option>
+              ))}
+          </Select>
+          <CustomRadarChart style={{width: 100, height: 133}}/>
+      </>
     )
 }
 
